df923f4ad165d5b5af131f9b1c394d588312fcd9
Turki <dev@turki.tech>
2021-02-24 23:34:01 +0300

    Addeing style to the memes

--- Memes.js
+++ Memes.js
@@ -1,11 +1,21 @@
+function createTitleElement(imgTitle) {
+    let titleElement = document.createElement('h1');
+    titleElement.className = 'memes-title';
+    titleElement.innerText = imgTitle;
+    console.log(titleElement);
+    let container = document.getElementById("container");
+    container.appendChild(titleElement);
+}
+
 function createImgElement(imgSrc) {
     let imgElement = document.createElement('img');
     imgElement.className = 'memes-images';
     imgElement.setAttribute('src', imgSrc)
     console.log(imgElement);
-    let container = document.getElementById("memes-container");
+    let container = document.getElementById("container");
     container.appendChild(imgElement);
 }
+
 
 fetch('https://www.reddit.com/r/memes/.json')
     .then((response) => response.json())
@@ -13,8 +23,9 @@ fetch('https://www.reddit.com/r/memes/.json')
         console.log(data)
         let memes = data.data.children;
         memes.map((meme) => {
-            console.log(meme);
-            createImgElement(meme.data.url)
+            // console.log(meme);
+            createTitleElement(meme.data.title)
+            createImgElement(meme.data.url_overridden_by_dest)
         })
     })
      .catch(error => console.log(error));
